b5bd7f109e688957427a37c339d7fb3d17f70ac7
Leon1435 <leonidasdamrong0828@gmail.com>
2026-03-12 17:41:21 +0800

    Organize the files

--- Data/Migrations/20260312092016_InitialCreate.Designer.cs
+++ Data/Migrations/20260312092016_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FYP_Project_II.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260311091229_InitialCreate")]
+    [Migration("20260312092016_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
